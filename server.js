0f8083467588b81cf03f418f6fe153b90477853c
Salar Khan <85280454+salaarkhan53@users.noreply.github.com>
2026-09-24 05:08:02 +0500

    Initialize Next.js server in server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const { createServer } = require('http');
+const next = require('next');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const hostname = '0.0.0.0';
+
+const app = next({
+  dev: false,
+  hostname,
+  port,
+});
+
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, hostname, () => {
+    console.log(`> Next.js server running on port ${port}`);
+  });
+});
